254640004faa4dcfa54942f6a1e220f340eb738b
ZoeLiao <zoejoyuliao@gmail.com>
2019-12-20 00:17:11 +0800

    Add Mutations

--- backend/myproject/myapp/schema.py
+++ backend/myproject/myapp/schema.py
@@ -3,16 +3,43 @@ import graphene
 from myapp.models import UserModel
 
 
-class User(DjangoObjectType):
+class UserType(DjangoObjectType):
     class Meta:
         model = UserModel
 
 
 class Query(graphene.ObjectType):
-    users = graphene.List(User)
+    users = graphene.List(UserType)
 
     def resolve_users(self, info):
         return UserModel.objects.all()
 
 
-schema = graphene.Schema(query=Query)
+class CreateUser(graphene.Mutation):
+    id = graphene.Int()
+    name = graphene.String()
+    last_name = graphene.String()
+
+    class Arguments:
+        name = graphene.String()
+        last_name = graphene.String()
+
+    def mutate(self, info, name, last_name):
+        user = UserModel(name=name, last_name=last_name)
+        user.save()
+
+        return CreateUser(
+            id=user.id,
+            name=user.name,
+            last_name=user.last_name,
+        )
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+
+schema = graphene.Schema(
+    query=Query,
+    mutation=Mutation
+)
